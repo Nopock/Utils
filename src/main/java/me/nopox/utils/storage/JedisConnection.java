d4a2e6e697c598b5d2586bca155e9cac15496373
Nopock <78834703+Nopock@users.noreply.github.com>
2022-05-30 09:46:30 -0700

    Implemented redis

--- src/main/java/me/nopox/utils/storage/JedisConnection.java
+++ src/main/java/me/nopox/utils/storage/JedisConnection.java
@@ -1,6 +1,26 @@
 package me.nopox.utils.storage;
 
+import lombok.Getter;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Getter
 public class JedisConnection {
 
-    //TODO: Implement
+    @Getter private static JedisConnection instance;
+
+    private final Jedis jedisResource;
+    private final JedisPool jedisPool;
+
+    public JedisConnection(String uri) throws URISyntaxException {
+        this.jedisPool = new JedisPool(new URI(uri));
+        this.jedisResource = jedisPool.getResource();
+        jedisResource.connect();
+        System.out.println("[Jedis] Connected to " + uri);
+    }
 }
+
+
--- src/main/java/me/nopox/utils/storage/repository/redis/RedisRepository.java
+++ src/main/java/me/nopox/utils/storage/repository/redis/RedisRepository.java
@@ -1,25 +1,52 @@
 package me.nopox.utils.storage.repository.redis;
 
+import me.nopox.utils.Utils;
+import me.nopox.utils.storage.JedisConnection;
 import me.nopox.utils.storage.repository.Repository;
+import redis.clients.jedis.Jedis;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.concurrent.CompletableFuture;
 
-public abstract class RedisRepository<K extends String, T extends Class<T>> implements Repository<K, T> {
+/**
+ * @author 98ping
+ *
+ * @param <K> The key that will be used to store the object
+ * @param <T> The type of the object that will be stored
+ */
+public abstract class RedisRepository<K extends String, T> implements Repository<K, T> {
+    private final String key;
 
-    //TODO: Implement
+    public RedisRepository(String key) {
+        this.key = key;
+    }
+
+    JedisConnection jedis = JedisConnection.getInstance();
 
     @Override
     public void save(K key, T value) {
+        CompletableFuture.runAsync(() -> {
+            Jedis jedis = this.jedis.getJedisResource();
 
+            jedis.hset(this.key, key, Utils.getInstance().getGSON().toJson(value));
+        });
     }
 
     @Override
     public CompletableFuture<T> byId(K id) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            Jedis jedis = this.jedis.getJedisResource();
+
+            return Utils.getInstance().getGSON().fromJson(jedis.hget(this.key, id), (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[1]);
+        });
     }
 
     @Override
     public CompletableFuture<Boolean> exists(K id) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            Jedis jedis = this.jedis.getJedisResource();
+
+            return jedis.hexists(this.key, id);
+        });
     }
 }
