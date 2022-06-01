fd422cfd2a06ddef9575119837534f8ef787953b
RealRepation <106334430+RealRepation@users.noreply.github.com>
2022-06-01 11:51:05 -0700

    Switched it to cache a CachedMethod.class
    This saves memory and doesn't need to save every single object inside the Mesasge.class because the message has a;ready been deleted

--- src/main/java/me/nopox/utils/discord/cache/message/MessageCacheListener.java
+++ src/main/java/me/nopox/utils/discord/cache/message/MessageCacheListener.java
@@ -8,8 +8,17 @@ public class MessageCacheListener extends ListenerAdapter {
 
     public void onMessageReceived(MessageReceivedEvent event) {
         if (!DiscordBot.isMessageCache()) return;
+        
+        CachedMessage m = new CachedMessage();
+        
+        m.setAuthor(event.getMember().getId());
+        m.setContent(event.getMessage().getContentRaw());
+        m.setTimeSent(system.currenttimemillis());
+        m.setGuild(event.getGuild().getId());
+        m.setChannel(event.getChannel().getId());
+        m.setChannelType(event.getChannel().getChannelType());
 
-        DiscordBot.getMessageCacheInstance().saveExpireable(event.getMessage().getId(), event.getMessage(), 60 * 60 * 24 * 7);
+        DiscordBot.getMessageCacheInstance().saveExpireable(event.getMessage().getId(), m, 60 * 60 * 24 * 7);
     }
     
     //TODO Remove it from redis after it has been deleted. Not sure how I will handle that ngl... might have to make the user remove it from the cache because if I just remove it here then I believe when they try and access it then it will be null...
