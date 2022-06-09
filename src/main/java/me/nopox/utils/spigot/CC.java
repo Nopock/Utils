d6aba760f9448b8fbf08c9ed3601ed5189047a08
Nopock <78834703+Nopock@users.noreply.github.com>
2022-06-09 01:27:21 -0700

    Added translate hex code

--- src/main/java/me/nopox/utils/spigot/CC.java
+++ src/main/java/me/nopox/utils/spigot/CC.java
@@ -3,12 +3,11 @@ package me.nopox.utils.spigot;
 import org.bukkit.ChatColor;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CC {
 
-    public static String translate(String text) {
-        return ChatColor.translateAlternateColorCodes('&', text);
-    }
 
     public static String[] translate(String[] text) {
         for (int i = 0; i < text.length; i++) {
@@ -22,5 +21,24 @@ public class CC {
             text.set(i, translate(text.get(i)));
         }
         return text;
+    }
+
+    public static String translate(String message) {
+        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+        Matcher matcher = pattern.matcher(message);
+        while (matcher.find()) {
+            String hexCode = message.substring(matcher.start(), matcher.end());
+            String replaceSharp = hexCode.replace('#', 'x');
+
+            char[] ch = replaceSharp.toCharArray();
+            StringBuilder builder = new StringBuilder("");
+            for (char c : ch) {
+                builder.append("&").append(c);
+            }
+
+            message = message.replace(hexCode, builder.toString());
+            matcher = pattern.matcher(message);
+        }
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
